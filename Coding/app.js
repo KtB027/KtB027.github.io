70fb92cac19a42eba41d5e99457f64ceb919b02a
KtB027 <99703083+KtB027@users.noreply.github.com>
2022-09-30 20:57:10 +0700

    Add files via upload

---
+++ Coding/app.js
@@ -0,0 +1,34 @@
+document.querySelector("#masuk").addEventListener("click",function(){
+	document.querySelector(".popup").classList.add("active");
+	document.querySelector(".popup-register").classList.remove("active");
+
+});
+
+document.querySelector(".popup .close-btn").addEventListener("click",function(){
+	document.querySelector(".popup").classList.remove("active");
+});
+
+document.querySelector("#registration").addEventListener("click",function(){
+	document.querySelector(".popup").classList.remove("active");
+	document.querySelector(".popup-register").classList.add("active");
+
+});
+
+document.querySelector(".popup-register .close-btn-register").addEventListener("click",function(){
+	document.querySelector(".popup-register").classList.remove("active");
+});
+
+document.querySelector("#login-back").addEventListener("click",function(){
+	document.querySelector(".popup").classList.add("active");
+	document.querySelector(".popup-register").classList.remove("active");
+
+});
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+	anchor.addEventListener("click",function(e){
+		e.preventDefault();
+		document.querySelector(this.getAttribute("href")).scrollIntoView({
+		 behavior : "smooth"
+		});
+	});
+});
